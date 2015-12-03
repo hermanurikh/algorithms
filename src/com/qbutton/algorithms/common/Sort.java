c061b3b242d00d99e7fa12dabd82aeff2769ba1a
German Urikh <herman.urikh@aengel.ru>
2015-12-03 23:52:10 +0400

    SelectionSort updated.

--- src/com/qbutton/algorithms/common/Sort.java
+++ src/com/qbutton/algorithms/common/Sort.java
@@ -4,5 +4,9 @@ package com.qbutton.algorithms.common;
  * A base abstract class with common methods.
  */
 public abstract class Sort {
+    /**
+     * The main sort method to be implemented.
+     * @param array the array to be sorted
+     */
     protected abstract void sort(int[] array);
 }
--- src/com/qbutton/algorithms/implementations/SelectionSort.java
+++ src/com/qbutton/algorithms/implementations/SelectionSort.java
@@ -1,10 +1,18 @@
 package com.qbutton.algorithms.implementations;
 
+import com.qbutton.algorithms.common.Sort;
+
 /**
  * Selection sort algorithm implementation (сортировка выбором).
  *
  * @author German Urikh
  * @since version 1.0 on 03.12.15.
  */
-public class SelectionSort {
+public class SelectionSort extends Sort {
+    /**
+     * {@inheritDoc}
+     */
+    public void sort(int[] a) {
+        //TODO implement
+    }
 }
