f06cb885f1434d7c7ca850daa479099b09185652
German Urikh <herman.urikh@aengel.ru>
2016-01-03 14:10:57 +0400

    GnomeSortAlgorithm and test implemented.

---
+++ src/main/java/com/qbutton/algorithms/implementations/GnomeSortAlgorithm.java
@@ -0,0 +1,41 @@
+package com.qbutton.algorithms.implementations;
+
+import com.qbutton.algorithms.common.BaseSortAlgorithm;
+import com.qbutton.algorithms.common.Measurable;
+import org.springframework.stereotype.Component;
+
+/**
+ * GnomeSortAlgorithm implementation (гномья сортировка).
+ *
+ * This algorithm checks if two elements are in wrong order. If so, they are swapped, and the
+ * cursor moves back one position, otherwise the cursor is equalised to maximum position cursor, and
+ * the latest is incremented.
+ *
+ * @author German Urikh
+ * @since version 1.0 on 03.01.16.
+ */
+@Component
+public class GnomeSortAlgorithm extends BaseSortAlgorithm {
+    /**
+     * {@inheritDoc}
+     */
+    @Measurable
+    @Override
+    public void sort(int[] array) {
+        checkNotNull(array);
+        int i = 1;
+        int j = 2;
+        while (i < array.length) {
+            if (array[i] < array[i - 1]) {
+                swap(array, i, i - 1);
+                i--;
+                if (i == 0) {
+                    i = j++;
+                }
+            } else {
+                i = j++;
+            }
+        }
+
+    }
+}
