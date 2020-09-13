2ed9836c5d3ca93543917243b04bb504af88000d
German Urikh <herman.urikh@aengel.ru>
2020-09-13 12:56:46 +0200

    Added new UnionSet with sizes.

--- src/main/java/com/qbutton/structures/SizedUnionSet.java
+++ src/main/java/com/qbutton/structures/SizedUnionSet.java
@@ -1,35 +1,41 @@
 package com.qbutton.structures;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 
 Time - O(alpha (n)), where alpha(n) for all practical cases is <= 4, so pretty much O(1). See CLRS.
 Space - O(n)
 
  */
-public class UnionSet {
+public class SizedUnionSet {
     int[] parent;
     int[] rank;
+    Map<Integer, Integer> sizes = new HashMap<>();
 
-    public UnionSet(int size) {
+    public SizedUnionSet(int size) {
         parent = new int[size];
         for (int i = 0; i < size; i++) {
             parent[i] = i;
+            sizes.put(i,1);
         }
         rank = new int[size];
 
     }
 
-    public int find(int num) {
+    public int findRepresentative(int num) {
         if (parent[num] != num) {
-            parent[num] = find(parent[num]);
+            parent[num] = findRepresentative(parent[num]);
         }
         return parent[num];
     }
 
-    public boolean union(int x, int y) {
-        int xr = find(x), yr = find(y);
+    public int union(int x, int y) {
+        int xr = findRepresentative(x), yr = findRepresentative(y);
+        int s1 = sizes.get(xr), s2 = sizes.get(yr);
         if (xr == yr) {
-            return false;
+            return sizes.get(xr);
         } else if (rank[xr] < rank[yr]) {
             parent[xr] = yr;
         } else if (rank[xr] > rank[yr]) {
@@ -38,6 +44,9 @@ public class UnionSet {
             parent[yr] = xr;
             rank[xr]++;
         }
-        return true;
+        int totalSize= s1 + s2;
+        sizes.put(xr, totalSize);
+        sizes.put(yr, totalSize);
+        return totalSize;
     }
 }
